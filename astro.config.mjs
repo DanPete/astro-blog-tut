7308cc1af108e0d68cb6114105b745d2228a33e6
Dan Sack <dan.sack@halfhelix.com>
2024-04-02 16:13:29 +0200

    Adds tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), alpinejs()],
+  site: "https://main--dan-astro-blog-tut.netlify.app/",
 });
